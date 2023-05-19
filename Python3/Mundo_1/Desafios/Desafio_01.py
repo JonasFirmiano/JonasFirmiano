c5447a4338a5a27966df7c28000f7dc0c086d429
JonasFirmiano <132873516+JonasFirmiano@users.noreply.github.com>
2023-05-19 00:39:22 -0300

    Adicionando pasta já criada.

---
+++ Python3/Mundo_1/Desafios/Desafio_01.py
@@ -0,0 +1,6 @@
+n1 = int(input('Primeiro Número: '))
+n2 = int(input('Segundo Número:'))
+
+soma = (n1 + n2)
+
+print(soma)
---
+++ Python3/Mundo_1/Exercicios/Ex_01.py
@@ -0,0 +1,3 @@
+nome = input('Qual o seu nome? ')
+
+print('Prazer em conhece-lo {}! Seja bem vindo.'.format(nome))
